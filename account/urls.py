ae1dcfdff8762cbabe4ade1d1b225c6fd42a507e
Yerassil <yerassil@gmail.com>
2020-05-10 17:10:01 +0600

    Change login/logout urls to built-in

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -7,8 +7,9 @@ urlpatterns = [
     # post views
     # path('login/', views.user_login, name='login'),
     path('', views.dashboard, name='dashboard'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('django.contrib.auth.urls')),
     # change password urls
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
