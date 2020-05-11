ac5c1fd2d94b14e20637cb5dc81fa9088dbc9b8e
Yerassil <yerassil@gmail.com>
2020-05-11 22:14:53 +0600

    Add social authentication

--- django_bookmarks/settings.py
+++ django_bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jcn+n$lq427&y*on@8z3+hh#=2016-pdpi%j%_p6oe(y-gnb94'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
 ]
 
 MIDDLEWARE = [
