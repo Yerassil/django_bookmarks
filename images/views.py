d12807f1a5ab647bf0d06b76162f01a1c05fd6fa
Yerassil <yerassil@gmail.com>
2020-05-21 22:23:40 +0600

    Add custom decorator (ajax_required) for image_like view

--- images/views.py
+++ images/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .forms import ImageCreateForm
 from .models import Image
+from common.decorators import ajax_required
 
 
 @login_required
@@ -39,6 +40,7 @@ def image_detail(request, id, slug):
                    'image': image})
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
