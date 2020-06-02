1228bba79a6724c63baadbcbd4305d8eb20896d3
Yerassil <yerassil@gmail.com>
2020-06-02 23:25:26 +0600

    Add image views counter using Redis

--- images/views.py
+++ images/views.py
@@ -9,6 +9,14 @@ from common.decorators import ajax_required
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+
+# Connect to Redis.
+r = redis.StrictRedis(host=settings.REDIS_HOST,
+                      port=settings.REDIS_PORT,
+                      db=settings.REDIS_DB)
 
 
 @login_required
@@ -38,10 +46,11 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr('image:{}:views'.format(image.id))
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image, 'total_views': total_views})
 
 
 @ajax_required
